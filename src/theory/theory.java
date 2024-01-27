8183ea094e599b504bf7cc627c3a777a175f04c9
Samuel <samugit.projects@gmail.com>
2024-01-26 22:15:48 -0300

    Aula01 Introdução ao conceito de serialização

---
+++ src/theory/theory.java
@@ -0,0 +1,30 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class theory {
+
+    /*
+    *
+    * Serialization
+    *
+    * Consiste em transformar um ou mais objetos da aplicação em um array de bytes com intuito de transferí-lo para algum lugar, exemplo, para um arquivo, ou pela rede
+    * O inverso também acontece quando recuperamos o que foi salvo que é transformar esse array de bytes no objeto do heap (Deserialization)
+    * Todos esses fluxos consistem em operações de I/O
+    *
+    *        I/O        Serialization
+    * Heap                    ^             Deserialization
+    *                         |
+    *        O1               |              ARQUIVO
+    *       / \         _______________
+    *     O2   O3   -> |__|__|__|__|__|  ->  NETWORK
+    *      |            array de bytes
+    *     O4
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * */
+}

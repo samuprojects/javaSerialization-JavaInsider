4f357a653a16ce4048b277e908eda1004b9d1b57
Samuel <samugit.projects@gmail.com>
2024-02-07 15:23:53 -0300

    Aula08 Entendendo o serialVersionUID (Parte 2/2)

--- src/serialization/Client.java
+++ src/serialization/Client.java
@@ -1,11 +1,22 @@
 package serialization;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
 @SuppressWarnings("ALL")
 public class Client implements Serializable { // incluindo a implementação para serializar objetos, toda a hierarquia deve ser serializada senão falhará
+
+
+    // para resolver o problema do serialVersionUID podemos adicionar como abaixo com qualquer valor, manualmente.
+    @Serial // usar essa anotação vai permitir que os compiladores executem alguma lógica ou verificação para checar ao que foi anotado. É uma boa prática
+    private static final long serialVersionUID = 1L;
+    // importante que alterar um nome de campo ou qualquer outra coisa pode trazer problemas, usar o serialVersionUID com parcimônia
+    // pricipalmente no cenário de exclusão ou mudança de nome
+    // por exemplo se alterar o id após a serialização e renomear para identification, na desserialização esse id virá com 0, pois o Java não sabe que deveria transformar essa informação
+    // se adicionar um campo extra, na desserialização virá como null
+
     private final int id; // tipos primitivos são serializados por padrão
     private final String name; // String implementa serializable
     private final LocalDate birthDate; // LocalDate implementa serializable

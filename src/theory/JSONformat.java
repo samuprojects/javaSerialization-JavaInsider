9d1f8890daae9f079676db4e37c6b291406d5eba
Samuel <samugit.projects@gmail.com>
2024-02-07 18:35:32 -0300

    Aula10 Introdução ao formato JSON

---
+++ src/theory/JSONformat.java
@@ -0,0 +1,32 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class JSONformat {
+
+    /*
+    *
+    * JavaScript Object Notation (JSON)
+    *
+    * É um formato de representação de objeto que veio da linguagem JavaScript
+    *
+    * Por ser muito conciso, flexível e expressivo foi adotado por outras linguagens de programação.
+    *
+    * É um formato independente de plataforma podendo ser produzidos ou consumidos por plataformas heterogeneas
+    *
+    * Consistem em uma String que representa objetos
+    *
+    * Possui um conjunto reduzido de tipos de dados, por exemplo, o objeto é representado por chaves
+    * Para nomes utilizamos aspas duplas, para arrays utilizamos colchetes, para números não precisa de nada e a separação entre elementos somente a vírgula
+    *
+    *      {
+    *       "NAME": "PEDRO",
+    *       "AGE": 30,
+    *       "CHILDREN": [7,5,6]
+    *      }
+    *
+    * Muito utilizado para serializar os objetos e geralmente utilizamos APIs para trabalhar com JSON
+    * No curso utilizaremos a API Jackson para manipulação do formato JSON (transformando objetos em JSON ou uma String no formato JSON e transformar em objetos do Java)
+    *
+    * O Jackson traz algumas flexibilidades na forma como esse mapeamento objeto JSON / JSON objeto
+    * */
+}

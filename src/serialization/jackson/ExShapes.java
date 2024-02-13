f283be7eddffc2e2738a3c43207366e26fa059e5
Samuel <samugit.projects@gmail.com>
2024-02-13 12:13:23 -0300

    Aula29 Exercício: criando figuras geométricas configuradas por JSON (Parte 2/2)

--- src/serialization/jackson/ExShapes.java
+++ src/serialization/jackson/ExShapes.java
@@ -19,6 +19,25 @@ public class ExShapes {
 
     public static void main(String[] args) {
 
+        ShapesConfig shapesConfig = ShapesConfig .create();
+
+        ShapeConfig rectangleConfig = shapesConfig.getConfig(ShapesConfig.ShapeType.RECTANGLE);
+
+        var rectangle = new Rectangle(
+                Integer.parseInt(rectangleConfig.getProperties().get("width")),
+                Integer.parseInt(rectangleConfig.getProperties().get("height"))
+        );
+        System.out.println(rectangle);
+
+        ShapeConfig squareConfig = shapesConfig.getConfig(ShapesConfig.ShapeType.SQUARE);
+        var square = new Square(Integer.parseInt(squareConfig.getProperties().get("size")));
+        System.out.println(square);
+
+        ShapeConfig circleConfig = shapesConfig.getConfig(ShapesConfig.ShapeType.CIRCLE);
+        var circle = new Circle(Integer.parseInt(circleConfig.getProperties().get("radius")));
+        System.out.println(circle);
+
+        // é um exemplo de como utilizar um arquivo JSON como uma fonte de configuração para criar objetos dentro da aplicação.
     }
 
     // records para criar os objetos
@@ -30,33 +49,36 @@ public class ExShapes {
     private record Circle(int radius) { }
 
 
-    // classe para representar as configurações dos elementos utilizando um enum
-    private static class ShapesConfig{
-        public enum ShapeType{
-            SQUARE, RECTANGLE, CIRCLE
-        }
+    /**
+     * @param configs atributo lista para representar uma configuração específica com um ShapeConfig
+     */ // classe para representar as configurações dos elementos utilizando um enum
+        private record ShapesConfig(List<ShapeConfig> configs) { // encapsula todos os configs
+            public enum ShapeType {
+                SQUARE, RECTANGLE, CIRCLE
+            }
 
-        private ShapesConfig(List<ShapeConfig> configs) {
-            this.configs = configs;
-        }
+        public static ShapesConfig create() {
+                try {
+                    return new ShapesConfig(
+                            JsonUtils.mapper().readValue(new File("shapesConfig.json"), new TypeReference<>() {})
+                    );
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
 
-        // atributo lista para representar uma configuração específica com um ShapeConfig
-        private final List<ShapeConfig> configs;
+            }
 
-        public ShapesConfig create(){
-            try {
-                return new ShapesConfig(
-                        JsonUtils.mapper().readValue(new File("shapesConfig.json"), new TypeReference<>() {})
-                );
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            public ShapeConfig getConfig(ShapeType type) { // método utilitário para retornar a informação de determinado shape
+                return configs
+                        .stream()
+                        .filter(c -> c.type == type)
+                        .findFirst()
+                        .orElseThrow();
             }
 
         }
 
-    }
-
-    private static class ShapeConfig {
+    private static class ShapeConfig { // representa a configuração de 1 shape, que é a estrutura que contém o type (enum) e properties (que é o Map) mapeia uma String Propriedade com outra String que é o Valor
         private ShapesConfig.ShapeType type; // enum
         private Map<String, String> properties;
 
@@ -66,6 +88,14 @@ public class ExShapes {
 
         public Map<String, String> getProperties() {
             return properties;
+        }
+
+        @Override
+        public String toString() {
+            return "ShapeConfig{" +
+                    "type=" + type +
+                    ", properties=" + properties +
+                    '}';
         }
     }
 }
